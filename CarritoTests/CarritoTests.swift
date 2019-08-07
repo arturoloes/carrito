6ef38e6f238c21907727eff6b84af082beb15a21
Lòpez Estrada Arturo <arturoloes@gmail.com>
2019-08-07 15:00:50 -0500

    termino

---
+++ CarritoTests/CarritoTests.swift
@@ -0,0 +1,34 @@
+//
+//  CarritoTests.swift
+//  CarritoTests
+//
+//  Created by Lòpez Estrada Arturo D3 on 5/9/19.
+//  Copyright © 2019 Lòpez Estrada Arturo. All rights reserved.
+//
+
+import XCTest
+@testable import Carrito
+
+class CarritoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
